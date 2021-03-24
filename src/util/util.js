dd382db67bc4c5780a8d0d8b5ac62dd0e0efae3a
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-03-24 20:27:23 +0400

    Add debugAlert util.

--- src/util/util.js
+++ src/util/util.js
@@ -1,3 +1,31 @@
+import {Alert} from 'react-native';
+
 export function isEmptyString(string) {
 	return !!string && string.length > 0;
+}
+
+export function debugAlert(error) {
+	let text = '';
+
+	if (typeof error == 'string') {
+		text = error;
+	} else if (typeof error == 'object') {
+		if (error instanceof Error) {
+			text = error.message;
+		} else {
+			text = JSON.stringify(error);
+		}
+	}
+
+	Alert.alert(
+		'Confirm:',
+		text,
+		[
+			{
+				style: 'cancel',
+				text: 'Cancel',
+			},
+		],
+		{cancelable: true}
+	);
 }
